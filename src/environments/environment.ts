3a83aeb85c2e7946426897438db492c3bc695bea
elpolakopolania <gustav0796@hotmail.com>
2024-08-28 16:49:29 +0930

    Config Login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,3 +2,12 @@ export const environment = {
   production: false,
   apiUrl: 'http://127.0.0.1:8000/api'
 };
+/*
+* Client ID: 1
+Client secret: Mt42zwpn1bA4zCq43wENYzsbo66yj3Tl4slCps68
+Password grant client created successfully.
+Client ID: 2
+Client secret: osTIyPOdTvG6Vpd77hiHWdHo6YkE38TmhCCRDgf3
+
+* */
+
