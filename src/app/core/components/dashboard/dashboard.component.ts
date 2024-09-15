cb5e47b400f39619d95c7cf3f0559c5616c9445f
elpolakopolania <gustav0796@hotmail.com>
2024-09-15 21:52:03 +0930

    Config dashboard component

--- src/app/core/components/dashboard/dashboard.component.ts
+++ src/app/core/components/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {EmployeeService} from "../../../services/employee/employee.service";
 import { CommonModule } from '@angular/common';
+import {ShiftService} from "../../../services/shift/shift.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -12,18 +13,18 @@ import { CommonModule } from '@angular/common';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
-  employees: any[] = [];
+  shifts: any[] = [];
 
-  constructor(private employeeService: EmployeeService) {
+  constructor(private shiftService: ShiftService) {
   }
 
   ngOnInit(): void {
-    this.employeeService.getEmployees().subscribe(
+    this.shiftService.getShifts().subscribe(
       data => {
-        this.employees = data;
+        this.shifts = data;
       },
       error => {
-        console.error('Error al obtener los empleados', error);
+        console.error('Error al obtener los turnos', error);
       }
     );
   }
