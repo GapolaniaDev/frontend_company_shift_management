134347993145c648867d2868b6666d32c339fdc7
Gustavo Polania <gustav0796@hotmail.com>
2025-02-16 22:27:37 +1030

    Configuration menu

--- src/app/main-layout/main-layout.component.ts
+++ src/app/main-layout/main-layout.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit,NgModule} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, OnInit, NgModule, Renderer2} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {RouterLink, RouterOutlet, Router} from "@angular/router";
 import {LoginService} from "../session/services/login.service";
+import {MenuComponent} from "../core/components/menu/menu.component";
 
 @Component({
   selector: 'app-main-layout',
@@ -9,20 +10,43 @@ import {LoginService} from "../session/services/login.service";
   imports: [
     RouterLink,
     RouterOutlet,
-    CommonModule
+    CommonModule,
+    MenuComponent
   ],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
 export class MainLayoutComponent implements OnInit {
   isLoggedIn: boolean | undefined;
+  isDarkMode = false;
+  menuOpen = false;
 
-  constructor(private loginService: LoginService, private router: Router) {
+  constructor(private loginService: LoginService, private router: Router, private renderer: Renderer2) {
 
   }
 
   ngOnInit() {
+    this.isDarkMode = localStorage.getItem('darkMode') === 'true';
+    this.updateTheme();
     this.isLoggedIn = this.loginService.isLoggedIn();
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
+    this.updateTheme();
+  }
+
+  onMenuOpenChange(menuOpen: boolean) {
+    this.menuOpen = menuOpen; // Actualizamos el estado de menuOpen
+  }
+
+  private updateTheme() {
+    if (this.isDarkMode) {
+      this.renderer.addClass(document.documentElement, 'dark');
+    } else {
+      this.renderer.removeClass(document.documentElement, 'dark');
+    }
   }
 
   logout() {
