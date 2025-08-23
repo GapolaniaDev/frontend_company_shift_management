196003b4527d8aa3c2eb047def4d5f3f932e3ed4
Gustavo Polania <gustav0796@hotmail.com>
2025-08-23 18:54:57 +0930

    Deleted all components that are not being used

--- src/app/features/schedule/components/gantt-header/gantt-header.component.ts
+++ src/app/features/schedule/components/gantt-header/gantt-header.component.ts
@@ -1,13 +1,12 @@
-import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { GanttMonthHeaderComponent } from '../gantt-month-header/gantt-month-header.component';
-import { GanttDayHeaderComponent } from '../gantt-day-header/gantt-day-header.component';
-import { ViewMode } from '../shared-types';
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {GanttDayHeaderComponent} from '../gantt-day-header/gantt-day-header.component';
+import {ViewMode} from '../shared-types';
 
 @Component({
   selector: 'app-gantt-header',
   standalone: true,
-  imports: [CommonModule, GanttMonthHeaderComponent, GanttDayHeaderComponent],
+  imports: [CommonModule, GanttDayHeaderComponent],
   templateUrl: './gantt-header.component.html',
   styleUrl: './gantt-header.component.css'
 })
--- src/app/features/schedule/index.ts
+++ src/app/features/schedule/index.ts
@@ -1,10 +1,5 @@
 // Schedule components
 export * from './components/schedule.component';
-export * from './components/calendar-header/calendar-header.component';
-export * from './components/employee-list/employee-list.component';
-export * from './components/navigation-bar/navigation-bar.component';
-export * from './components/search-bar/search-bar.component';
-export * from './components/status-bar/status-bar.component';
 export * from './components/payment-periods/payment-periods.component';
 
 // Schedule data access
